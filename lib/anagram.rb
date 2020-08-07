edcb1790ec4a77bda4030cf4d7f0a60bd9f550e4
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-07 17:32:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+# My coooode
+
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(find_anagram)
+    find_anagram.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+  end 
+end
